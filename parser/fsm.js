06a876fbf402f728dbea7b948810d7edf26d79b0
Seven Lju <dna2gle@gmail.com>
2017-11-12 15:35:10 +0800

    [parser] add more features
    - add support python doc
    - add ruby heredoc, percent string
    - add common comment
       - e.g. start=/* end=*/
       -      start=// end=\n
       -      start==begin end=\n=end
       -      start=# end=\n

--- parser/fsm.js
+++ parser/fsm.js
@@ -71,12 +71,16 @@ class Feature {
       this.env.input = input;
       let output = [];
       let cursor = this.state[this.entry];
-      input.forEach((x, i) => {
+      for(let i = 0, n = input.length; i < n; ++i ) {
+         let x = input[i];
          this.env.input_i = i;
          cursor = cursor.next(x, this.env);
-         cursor.act(output, x, this.env);
+         let offset = cursor.act(output, x, this.env);
+         if (offset) {
+            i += offset - 1;
+         }
          cursor = cursor.state;
-      });
+      }
       delete this.env.input;
       delete this.env.input_i;
       return output;
@@ -94,28 +98,27 @@ class FeatureRoot extends Feature {
             });
          }, always)
       );
-      empty_state.bydefault.state = empty_state;
       this.register_state('epsilon', empty_state);
       this.set_entry('epsilon');
    }
 }
 
-class FeatureCStyleString extends Feature {
+class FeatureCommonString extends Feature {
    constructor(marks) {
       super();
       let string_state = new State(
-         new Condition(0, this._concat, always)
+         new Condition(0, act_concat, always)
       );
       let string_escape_state = new State(
-         new Condition(0, this._concat, always, string_state)
+         new Condition(0, act_concat, always, string_state)
       );
       string_state.register_condition(new Condition(
-         0, this._concat,
+         0, act_concat,
          (x, env) => x.token === '\\',
          string_escape_state
       ));
-      this.register_state('c_style_string', string_state);
-      this.register_state('c_style_string_escape', string_escape_state);
+      this.register_state('common_string', string_state);
+      this.register_state('common_string_escape', string_escape_state);
       this.marks = marks || ['\'', '"'];
       this.env.stop_mark = null;
       this.set_entry(null);
@@ -123,10 +126,10 @@ class FeatureCStyleString extends Feature {
 
    merge_feature_to(feature) {
       // merge to feature root
-      // connect c_style_string to epsilon
+      // connect common_string to epsilon
       if (!('epsilon' in feature.state)) return;
       let epsilon = feature.state.epsilon;
-      let string_state = this.state.c_style_string;
+      let string_state = this.state.common_string;
       epsilon.register_condition(new Condition(
          5, (output, x, env) => {
             output.push({
@@ -140,7 +143,7 @@ class FeatureCStyleString extends Feature {
       ));
       string_state.register_condition(new Condition(
          5, (output, x, env) => {
-            this._concat(output, x, env);
+            act_concat(output, x, env);
             env.stop_mark = null;
          }, (x, env) => {
             return env.stop_mark === x.token;
@@ -149,10 +152,273 @@ class FeatureCStyleString extends Feature {
       return feature;
    }
 
-   _concat(output, x, env) {
-      let m = last(output);
-      m.token += x.token;
+   merge_feature_as_python_doc_to(feature) {
+      // merge to feature root
+      // connect common_string to epsilon
+      if (!('epsilon' in feature.state)) return;
+      let epsilon = feature.state.epsilon;
+      let string_state = this.state.common_string;
+      epsilon.register_condition(new Condition(
+         5, (output, x, env) => {
+            // ''' """
+            let punc = x.token.repeat(3);
+            output.push({
+               token: punc,
+               tag: 'S'
+            });
+            env.stop_mark = punc;
+            return 3;
+         }, (x, env) => {
+            return (
+               contains(this.marks, x.token) &&
+               text_cmp_n(x.token.repeat(3), env.input, env.input_i, 3)
+            );
+         }, string_state
+      ));
+      string_state.register_condition(new Condition(
+         5, (output, x, env) => {
+            act_concat(output, { token: env.stop_mark }, env);
+            env.stop_mark = null;
+            return 3;
+         }, (x, env) => {
+            return (
+               contains(this.marks, x.token) &&
+               text_cmp_n(env.stop_mark, env.input, env.input_i, 3)
+            );
+         }, epsilon
+      ));
+      return feature;
    }
+}
+
+class FeatureCommonComment extends Feature {
+   constructor(state_name, start, end, include_end, tag) {
+      super();
+      let comment = new State(
+         new Condition(0, act_concat, always)
+      );
+      this.register_state(state_name, comment);
+      this.set_entry(null);
+      // e.g. start = ['/', '/'], end = ['\n'], include_end = false
+      //      start = ['/', '*'], end = ['*', '/'], include_end = true
+      if (Array.isArray(start)) {
+         this.env.start_len = start.length;
+         this.env.start = start.join('');
+      } else {
+         this.env.start_len = 1;
+         this.env.start = start;
+      }
+      if (Array.isArray(end)) {
+         this.env.end_len = end.length;
+         this.env.end = end.join('');
+      } else {
+         this.env.end_len = 1;
+         this.env.end = end;
+      }
+      this.env.include_end = include_end;
+      this.env.tag = tag || 'C';
+   }
+
+   merge_feature_to(feature) {
+      // merge to feature root
+      // connect common_string to epsilon
+      if (!('epsilon' in feature.state)) return;
+      let epsilon = feature.state.epsilon;
+      let comment = this.state[Object.keys(this.state)[0]];
+      let start = this.env.start;
+      let start_len = this.env.start_len;
+      let end = this.env.end;
+      let end_len = this.env.end_len;
+      let include_end = this.env.include_end;
+      let tag = this.env.tag;
+      epsilon.register_condition(new Condition(
+         5, (output, x, env) => {
+            output.push({ token: start, tag: tag });
+            return start_len;
+         }, factory_text_cmp(start, start_len), comment
+      ));
+      comment.register_condition(new Condition(
+         5, (output, x, env) => {
+            if (include_end) {
+               act_concat(output, { token: end }, env);
+            } else {
+               output.push({ token: end, tag: null });
+            }
+            return end_len;
+         }, factory_text_cmp(end, end_len), epsilon
+      ));
+      return feature;
+   }
+}
+
+class FeatureRubyHereDocString extends Feature {
+   // e.g. <<-EOF
+   //      hello world
+   //      EOF
+   constructor() {
+      super();
+      let string_state = new State(
+         new Condition(0, act_concat, always)
+      );
+      let select_state = new State(
+         new Condition(0, (output, x, env) => {
+            act_concat(output, x, env);
+            env.stop_mark = x.token;
+         }, always, string_state)
+      );
+      select_state.register_condition(
+         new Condition(5, act_concat, (x, env) => {
+            return contains(['\'', '"', '-', '`'], x.token);
+         }, select_state)
+      );
+      this.register_state('ruby_heredoc_string', string_state);
+      this.register_state('ruby_heredoc_string_start_select', select_state);
+      this.set_entry(null);
+   }
+
+   merge_feature_to(feature) {
+      // merge to feature root
+      // connect common_string to epsilon
+      if (!('epsilon' in feature.state)) return;
+      let epsilon = feature.state.epsilon;
+      let string_state = this.state.ruby_heredoc_string;
+      let select_state = this.state.ruby_heredoc_string_start_select;
+      epsilon.register_condition(new Condition(
+         5, (output, x, env) => {
+            output.push({
+               token: '<<',
+               tag: 'S'
+            });
+            return 2;
+         }, (x, env) => {
+            if (env.input_i+1 >= env.input.length) return false;
+            return x.token === '<' && env.input[env.input_i+1].token === '<';
+         }, select_state
+      ));
+      string_state.register_condition(new Condition(
+         5, (output, x, env) => {
+            act_concat(output, x, env);
+            env.stop_mark = null;
+         }, (x, env) => {
+            return env.stop_mark === x.token && env.input[env.input_i-1].token === '\n';
+         }, epsilon
+      ));
+      return feature;
+   }
+}
+
+class FeatureRubyPercentString extends Feature {
+   // e.g. %Q(h(e(l(l(o)w)o)r)ld)
+   constructor() {
+      super();
+      let string_state = new State(
+         new Condition(0, act_concat, always)
+      );
+      let string_escape_state = new State(
+         new Condition(0, act_concat, always, string_state)
+      );
+      string_state.register_condition(
+         new Condition(5, act_concat, (x, env) => {
+            return env.stop_mark !== '\\' && x.token === '\\';
+         }, string_escape_state)
+      );
+      this.register_state('ruby_percent_string', string_state);
+      this.register_state('ruby_percent_string_escape', string_escape_state);
+      this.set_entry(null);
+   }
+
+   merge_feature_to(feature) {
+      // merge to feature root
+      // connect common_string to epsilon
+      const starter = [
+         '`', '~', '!', '@', '#', '$', '%', '^',
+         '&', '*', '(', ')', '-', '_', '=', '+',
+         '[', ']', '{', '}', '\\', '|', ';', ':',
+         '\'', '"', '<', '>', ',', '.', '/', '?'
+      ];
+      const flag = ['q', 'Q', 'w', 'W', 'i', 'I', 'r', 'x', 's'];
+      if (!('epsilon' in feature.state)) return;
+      let epsilon = feature.state.epsilon;
+      let string_state = this.state.ruby_percent_string;
+      epsilon.register_condition(new Condition(
+         5, (output, x, env) => {
+            let token = '%' + env.flag + env.left_mark;
+            output.push({
+               token: token,
+               tag: 'S'
+            });
+            env.deep = 1;
+            switch(env.left_mark) {
+               case '(': env.stop_mark = ')'; break;
+               case '{': env.stop_mark = '}'; break;
+               case '[': env.stop_mark = ']'; break;
+               case '<': env.stop_mark = '>'; break;
+               default: env.stop_mark = env.left_mark;
+            }
+            return token.length;
+         }, (x, env) => {
+            if (x.token !== '%') return false;
+            let f1 = env.input[env.input_i+1];
+            let f2 = env.input[env.input_i+2];
+            if (!f1) return false;
+            if (contains(starter, f1.token)) {
+               env.left_mark = f1.token;
+               env.flag = '';
+               return true;
+            } else if (f2 && contains(flag, f1.token) && contains(starter, f2.token)) {
+               env.left_mark = f2.token;
+               env.flag = f1.token;
+               return true;
+            }
+            return false;
+         }, string_state
+      ));
+      string_state.register_condition(new Condition(
+         5, (output, x, env) => {
+            act_concat(output, x, env);
+            delete env.deep;
+            delete env.left_mark;
+            env.stop_mark = null;
+         }, (x, env) => {
+            if (x.token === env.stop_mark) {
+               env.deep --;
+               if (!env.deep) return true;
+               return false;
+            }
+            if (x.token === env.left_mark) {
+               env.deep ++;
+            }
+            return false;
+         }, epsilon
+      ));
+      return feature;
+   }
+}
+
+function act_concat(output, x, env) {
+   let m = last(output);
+   m.token += x.token;
+}
+
+function factory_text_cmp(expect, len) {
+   if (len === 1) {
+      return (x, env) => {
+         return text_cmp_1(expect, env.input, env.input_i);
+      }
+   } else {
+      return (x, env) => {
+         return text_cmp_n(expect, env.input, env.input_i, len);
+      }
+   }
+}
+
+function text_cmp_1(expect, input, index) {
+   return expect === input[index].token;
+}
+
+function text_cmp_n(expect, input, index, len) {
+   return expect === input.slice(
+      index, index + len).map((z) => z.token).join('');
 }
 
 function always() {
@@ -172,5 +438,8 @@ module.exports = {
    State,
    Feature,
    FeatureRoot,
-   FeatureCStyleString
+   FeatureCommonString,
+   FeatureCommonComment,
+   FeatureRubyHereDocString,
+   FeatureRubyPercentString
 };
--- test.js
+++ test.js
@@ -5,5 +5,25 @@ const tokenizer = require('./parser/tokenizer');
 let filename = process.argv[2] || 'test.js';
 let text = fs.readFileSync(filename).toString();
 let tokens = new tokenizer.WordTokenizer().process(text);
-tokens = new fsm.FeatureCStyleString().merge_feature_to(new fsm.FeatureRoot()).process(tokens);
+
+// test for common comment and string tokenizer
+/* define c style (multiple)' line comment */
+let parser = new fsm.FeatureRoot();
+let feature;
+feature = new fsm.FeatureCommonString(['\'', '"', '`']);
+//feature.merge_feature_as_python_doc_to(parser);
+feature.merge_feature_to(parser);
+feature = new fsm.FeatureCommonComment('c_style_line_comment', '//'.split(''), '\n', false);
+feature.merge_feature_to(parser);
+feature = new fsm.FeatureCommonComment('c_style_multiline_comment', '/*'.split(''), '*/'.split(''), true);
+feature.merge_feature_to(parser);
+//feature = new fsm.FeatureRubyHereDocString();
+//feature.merge_feature_to(parser);
+//feature = new fsm.FeatureRubyPercentString();
+//feature.merge_feature_to(parser);
+//feature = new fsm.FeatureCommonComment('ruby_style_line_comment', '#', '\n', false);
+//feature.merge_feature_to(parser);
+//feature = new fsm.FeatureCommonComment('ruby_multiline_comment', ['=', 'begin'], ['\n', '=', 'end'], true);
+//feature.merge_feature_to(parser);
+tokens = parser.process(tokens);
 console.log(JSON.stringify(tokens, null, 3));
