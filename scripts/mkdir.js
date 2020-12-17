208fa873618298dbe4b6b92e9e8b9fd867d7aa8b
Seven Lju <dna2gle@gmail.com>
2020-12-17 15:33:17 +0800

    export path api for metadata in mkdir.js

--- scripts/mkdir.js
+++ scripts/mkdir.js
@@ -1,11 +1,19 @@
 const i_fs = require('fs');
 const i_path = require('path');
 
+const api = {
+   path: (base_dir, relative_path) => {
+      const dir = i_path.join(base_dir, '.lazac', 'files', `${relative_path}.meta`);
+      return dir;
+   }
+};
+
 module.exports = {
+   api,
    one: (mtime, flag, size, name, path) => {
       if (flag !== 'F') return;
       console.log(name);
-      const dir = i_path.join(path, '.lazac', 'files', `${name}.meta`);
+      const dir = api.path(path, name);
       i_fs.mkdirSync(dir, { recursive: true });
-   }
+   },
 };
